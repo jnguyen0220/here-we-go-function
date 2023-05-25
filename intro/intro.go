5556adb9deb60fd40595a09a57c8c2261c89447e
JonnyN <49132940+jnguyen0220@users.noreply.github.com>
2023-05-25 08:25:33 -0500

    Update intro.go

--- intro/intro.go
+++ intro/intro.go
@@ -6,3 +6,7 @@ import (
 func Hello() {
   fmt.Println("Hello from github.com/jnguyen0220/here-we-go-function again 1234")
 }
+
+func Welcome() {
+  fmt.Println("Welcome from github.com/jnguyen0220/here-we-go-function")
+}
